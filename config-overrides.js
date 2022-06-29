4fe665588be1f46ea1942e143890ca94f1875a47
钟太松 <zhongtaisong@wanmi.com>
2022-06-29 21:34:15 +0800

    【webpack5】升级

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,7 @@
 const { 
     override, fixBabelImports, addLessLoader,
-    addWebpackAlias, addDecoratorsLegacy, addWebpackPlugin 
+    addWebpackAlias, addDecoratorsLegacy, addWebpackPlugin,
+    adjustStyleLoaders,
 } = require('customize-cra');
 const path = require('path');
 const webpack = require('webpack');
@@ -13,8 +14,11 @@ module.exports = override(
         style: true
     }),
     addLessLoader({
-        javascriptEnabled: true,
-        modifyVars: { '@primary-color': '#1890ff' }
+        lessOptions: {
+            javascriptEnabled: true,
+            modifyVars: { '@primary-color': '#1890ff' },
+            math: "always",
+        },
     }),
     addWebpackAlias({
         '@utils': path.resolve(__dirname, 'src/utils'),
@@ -27,9 +31,25 @@ module.exports = override(
         '@store': path.resolve(__dirname, 'src/store')
     }),
     addDecoratorsLegacy(),
-    addWebpackPlugin(new webpack.ProgressPlugin()),
+    addWebpackPlugin(new webpack.ProgressPlugin({
+        activeModules: false,
+        entries: true,
+        handler(percentage, message, ...args) {
+          console.info(`进度：${(percentage * 100)?.toFixed?.(2)}%`, message, ...args);
+        },
+        modules: true,
+        modulesCount: 5000,
+        profile: false,
+        dependencies: true,
+        dependenciesCount: 10000,
+        percentBy: null,
+    })),
     addWebpackPlugin(new MiniCssExtractPlugin({
         filename: 'static/css/[name].[contenthash:8].css',
         chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
     })),
+    adjustStyleLoaders(({ use: [, , postcss] }) => {
+        const postcssOptions = postcss.options;
+        postcss.options = { postcssOptions };
+    }),
 );
