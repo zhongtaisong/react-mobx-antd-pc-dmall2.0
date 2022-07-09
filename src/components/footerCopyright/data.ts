3d54a9c8428f9c7580275e1b33800b6c5e381a2a
zhongtaisong <zhongtaisong@sina.com>
2022-07-10 00:43:55 +0800

    【update】更新底部版权组件

---
+++ src/components/footerCopyright/data.ts
@@ -0,0 +1,68 @@
+/**
+ * 导航菜单
+ */
+export const FOOTER_MENU_LIST = [
+    { 
+        id: 1, 
+        title: '代码仓库', 
+        content: [
+            { 
+                cid: 1, 
+                name: 'Gitee',
+                url: 'https://gitee.com/zhongts', 
+            },
+            { 
+                cid: 2, 
+                name: 'Github',
+                url: 'https://github.com/zhongtaisong', 
+            }
+        ]
+    },
+    { 
+        id: 2, 
+        title: '技术栈', 
+        content: [
+            { 
+                cid: 1, 
+                name: 'Create-React-App',
+                url: 'https://create-react-app.dev/', 
+            },
+            { 
+                cid: 2, 
+                name: 'Mobx',
+                url: 'https://mobx.js.org/README.html', 
+            },
+            { 
+                cid: 3, 
+                name: 'Antd',
+                url: 'https://ant.design/components/overview-cn/', 
+            }
+        ]
+    },
+    { 
+        id: 3, 
+        title: '社区', 
+        content: [
+            { 
+                cid: 1, 
+                name: '掘金',
+                url: 'https://juejin.im/', 
+            },
+            { 
+                cid: 2, 
+                name: 'SegmentFault 思否',
+                url: 'https://segmentfault.com/', 
+            },
+            { 
+                cid: 3, 
+                name: 'GitHub',
+                url: 'https://github.com/', 
+            },
+            { 
+                cid: 4, 
+                name: 'Gitee',
+                url: 'https://gitee.com/', 
+            },
+        ]
+    },
+];
--- src/components/footerCopyright/index.tsx
+++ src/components/footerCopyright/index.tsx
@@ -1,57 +1,35 @@
 import React from 'react';
 import { Row, Col } from 'antd';
 import { observer } from 'mobx-react';
+import { FOOTER_MENU_LIST } from './data';
 // less样式
 import './index.less';
 
-const footerData = [
-    { id: 1, title: 'Demo Mall', 
-        content: [
-            { cid: 1, url: '/views/web', name: '网站说明' },
-            { cid: 2, url: '/views/message', name: '留言' }
-        ]
-    },
-    { id: 2, title: '相关技术栈', 
-        content: [
-            { cid: 1, url: 'https://www.html.cn/create-react-app/docs/getting-started/', name: 'create-react-app' },
-            { cid: 2, url: 'https://cn.mobx.js.org/', name: 'mobx' },
-            { cid: 3, url: 'https://cn.mobx.js.org/', name: 'antd' }
-        ]
-    },
-    { id: 3, title: '技术社区', 
-        content: [
-            { cid: 1, url: 'https://juejin.im/', name: '掘金' },
-            { cid: 2, url: 'https://segmentfault.com/', name: 'SegmentFault 思否' },
-            { cid: 3, url: 'https://github.com/', name: 'GitHub' },
-            { cid: 4, url: 'https://www.csdn.net/', name: 'CSDN' }
-        ]
-    },
-    // { id: 4, title: '合作联系', 
-    //     content: [
-    //         { cid: 1, url: '/', name: '微信' },
-    //         { cid: 2, url: '/', name: 'QQ' },
-    //         { cid: 3, url: '/', name: '新浪邮箱' }
-    //     ]
-    // },
-]
-
-// 底部版权区域
+/**
+ * 底部版权区域
+ */
 @observer
 class FooterCopyright extends React.Component<any, any> {
-
     render() {
         return (
             <div className='dm_FooterCopyright'>
                 <Row className='common_width'>
                     {
-                        footerData.map(item01 => {
+                        FOOTER_MENU_LIST.map(item01 => {
                             return (
-                                <Col span={ 8 } key={ item01.id }>
+                                <Col 
+                                    key={ item01.id }
+                                    span={ 8 } 
+                                >
                                     <i>{ item01.title }</i>
                                     {
-                                        item01.content.map(item02 => {
+                                        item01?.content?.map?.(item02 => {
                                             return (
-                                                <a target='_blank' href={ item02.url } key={ item02.cid }>{ item02.name }</a>
+                                                <a 
+                                                    key={ item02.cid }
+                                                    target='_blank' 
+                                                    href={ item02.url } 
+                                                >{ item02.name }</a>
                                             )
                                         })
                                     }
@@ -60,11 +38,11 @@ class FooterCopyright extends React.Component<any, any> {
                         })
                     }
                 </Row>
-                <div className='copyright'>                    
-                    <div className='common_width'>
-                        闹钟太松了 2019-11-10 设计、开发、测试、上线
-                    </div>
-                </div>
+
+                <ul className='dm_FooterCopyright__copyright'> 
+                    <li>闹钟太松了 - 2019/11/10 - 设计、开发、测试、上线</li>
+                    <li>最近更新时间：{ new Date().toLocaleDateString() }</li>
+                </ul>
             </div>
         );
     }
